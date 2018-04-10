fd3bc3361aed152bd06244efc9b366f1a11b793b
Greg <farr.gs@gmail.com>
2018-04-10 08:39:23 +0200

    started skeleton of index.php

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-<?php // silence
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+  <meta name="description" content="">
+    <meta name="author" content="Greg Farr">
+<title>Online CV for Software Developers</title>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+   <!-- Custom styles for this template -->
+    <link href="<?php bloginfo('template_directory');?>/blog.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="container">
+
+Hello
+
+</div>
+
+
+</body>
+
+
+
+</html>
